dce84adf51c33e3a53824f7dfcc5aed21dcc8b4f
quqeiqa <glauberov@yandex.ru>
2015-10-16 18:26:15 +0500

    Update JSONObject.java

--- src/main/java/org/json/simple/JSONObject.java
+++ src/main/java/org/json/simple/JSONObject.java
@@ -34,7 +34,41 @@ public class JSONObject extends HashMap implements Map, JSONAware, JSONStreamAwa
 	public JSONObject(Map map) {
 		super(map);
 	}
-
+	
+	/**
++	 * Get the string value. If value is null (because not found?), returns @defaultValue
++	**/
++	public string getString(string key, string defaultValue="") {
++		Object value = get(key);
++		if((string)value == null) {
++			return defaultValue;
++		} else {
++			return (string)value;
++		}
++	}
++	
++	/**
++	 * Get the long int value. If value is null (because not found?), returns @defaultValue
++	**/
++	public long getLong(string key, long defaultValue = 0) {
++		Object value = get(key);
++		if(value == null)
++			return defaultValue;
++		else
++			return (long)value;
++	}
++	
++	/**
++	 * Get the double float value. If value is null (because not found?), returns @defaultValue
++	**/
++	public double getDouble(string key, double defaultValue = 0.0) {
++		Object value = get(key);
++		if(value == null)
++			return defaultValue;
++		else
++			return (double)value;
++	}
+ 	
 
     /**
      * Encode a map into JSON text and write it to out.
